aeabbc803988727636da4951c9db7ab33c81241a
Logan <Logan@kleditz.com>
2021-01-29 14:19:46 -0800

    Added the leap_year file without error handling

---
+++ ly_no.py
@@ -0,0 +1,22 @@
+#I am just doing python3 Logan_Kleditz_hw1.py, I hope that's ok
+def open_st():
+    x = input("Enter a year in the form of an integer: ")
+    x = int(x)
+    check(x)
+
+def check(x):
+    a = 0
+    if (x % 4) == 0:
+        a += 1
+        if (x % 100) == 0:
+            if (x % 400) == 0:
+                a += 1
+            else:
+                a -= 1
+    if a > 0:
+        print("{} was a leap year". format(x))
+    else:
+        print("{} wasn't a leap year". format(x))
+
+if __name__ == '__main__':
+    open_st()
